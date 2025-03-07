271a9231f6fd2fe375dccc0e5c7a8d8a2554cab9
restr <restr@JuanRestrepo>
2025-03-07 14:51:41 -0500

    mensajes por pantalla de cueunta

---
+++ src/main/java/com/mycompany/github/EjercicioHerencia.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.github;
+
+/**
+ *
+ * @author restr
+ */
+public class EjercicioHerencia {
+
+    public static void main(String[] args) {
+     // crear objeto de la clase cuenta 
+     cuenta cuenta1 = new cuenta (10000, 0);
+     cuenta1.imprimir();
+     
+     cuenta1.consignar(2501);
+     cuenta1.imprimir();
+     
+    }
+}
